e9101d3f78156e3157009fa057df2f288a99f698
killapop <killapop@protonmail.com>
2018-09-19 20:27:18 +0200

    initial

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-preact',
+    {
+      resolve: {
+        alias: {
+          react: 'preact-compat',
+          'react-dom': 'preact-compat',
+          // Not necessary unless you consume a module using `createClass`
+          'create-react-class': 'preact-compat/lib/create-react-class',
+          // Not necessary unless you consume a module requiring `react-dom-factories`
+          'react-dom-factories': 'preact-compat/lib/react-dom-factories',
+        },
+      },
+    },
+  ],
+}
