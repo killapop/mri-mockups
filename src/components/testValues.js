b057018f4efa8621a0aa002c36916cec0379563f
killapop <killapop@protonmail.com>
2018-09-24 16:04:25 +0200

    fix typo

--- src/components/testValues.js
+++ src/components/testValues.js
@@ -5,7 +5,7 @@ class TestValues extends React.Component {
   render() {
     return (
       <div className="test-values pa0 form-group-inline fixed left-0 bt b--light-silver bg-gray white pv2 w-10 shadow-light">
-        <div className="f6 ttu b db pa2">Text values</div>
+        <div className="f6 ttu b db pa2">Test values</div>
         <div className="form w-90 center">{this.props.children}</div>
       </div>
     )
